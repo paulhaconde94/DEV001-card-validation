beab572419f2bbd4f47e9c85edff59cf40cf42e1
Paula <paulhaconde@gmail.com>
2022-10-06 18:58:21 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-import validator from './validator.js';
+import validator from "./validator.js";
+//Selectores
+let NumeroDeTarjeta = document.getElementById("NumeroDeTarjeta")
+let NombreDeUsuario = document.getElementById("NombreDeUsuario")
+
+
+//Botón que genera una alerta al ingresar un dato, refiere verificada o no verficada
+let botonVerificar = document.getElementById("botonVerificar");
+
+botonVerificar.addEventListener("click", () => {
+    let numeroFinal = NumeroDeTarjeta.value
+    if(numeroFinal <= 16){
+      return alert("La tarjeta debe tener mínimo 14 numeros");
+      
+    } 
+
+//Nos muestra el nombre del usuario 
+document.getElementById("nombre-de-usuario-final"). innerHTML = NombreDeUsuario.value;
+
+//bloqueo de la segunda pantalla hasta que no se cumpla con los campos requeridos
+document.getElementById("contenedorTarjeta").style.display= "none";
+document.getElementById("SegundaPantalla").style.display= "block";
+
+});
+
+//recarga la segunda pantalla 
+const SegundaPantalla = document.getElementById('SegundaPantalla');
+
+SegundaPantalla.addEventListener('click', () =>{
+
+  Location.reload();
+
+  
+
+
+});
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,34 @@
+function isValid(numero_tarjeta){
+    let longitud = numero_tarjeta.lenght;
+
+    let cifra = null;
+    let cifra_cad = null;
+    let suma = 0;
+// Expresiones regulares en HTML /[^0-9-\s]+/
+// El método test() ejecuta la búsqueda de una ocurrencia entre una expresión regular y una cadena especificada. Devuelve true o false.
+let numValid = /[^0-9-\s]+/.test(numero_tarjeta);
+//Condicion para ingresar solo caracteres de tipo numerico
+if (numValid === true) {
+    return alert ("Ingrese solo caracteres de tipo numerico en este campo");
+}
+
+
+
+}
+
+
 const validator = {
-  // ...
+isValid,
+
+
+
+
 };
+
+
+
+
+ 
+
 
 export default validator;
